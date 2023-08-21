919e3e9e4a4b0890a064455242ed48c2e35ee598
23CSBMOHAMMEDUSMAN <139548202+MohammedUsman72@users.noreply.github.com>
2023-08-21 09:13:55 +0000

    completed

---
+++ area.java
@@ -0,0 +1,9 @@
+class area {
+    public static void main(String args[]){
+        double pi,r,a;
+        r = 10;
+        pi = 3.14159;
+        a = pi*r*r;
+        System.out.println("The area of circle with radius is " +a);
+    }
+}
